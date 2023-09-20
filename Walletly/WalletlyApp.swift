615d3e4c966b8948cbccbc3db3d46d5ff61afbbc
GkhKaya <gkhkaya00@gmail.com>
2023-09-20 23:05:11 +0300

    First Commit

---
+++ Walletly/WalletlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletlyApp.swift
+//  Walletly
+//
+//  Created by Gokhan Kaya on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WalletlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
