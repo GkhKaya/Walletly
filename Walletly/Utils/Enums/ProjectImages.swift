5364a59ba4979040364ee0a807960b07ef82d439
GkhKaya <gkhkaya00@gmail.com>
2023-10-01 18:07:49 +0300

    Added project images and localizable.

---
+++ Walletly/Utils/Enums/ProjectImages.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectImages.swift
+//  Walletly
+//
+//  Created by Gokhan Kaya on 1.10.2023.
+//
+
+import Foundation
+
+struct ImageItems{
+    enum GeneralImages : String{
+        case walletlyLogo = "walletly_logo"
+        case icGoogle = "ic_google"
+        case icApple = "icApple"
+        
+    }
+}
---
+++ Walletly/Utils/Locale/LocalKeys.swift
@@ -0,0 +1,14 @@
+//
+//  LocalKeys.swift
+//  Walletly
+//
+//  Created by Gokhan Kaya on 1.10.2023.
+//
+
+import SwiftUI
+
+struct LocalKeys {
+    enum Auth : String {
+        
+    }
+}
